f980da5208f13d96685e77e427d8acb1700556a6
donatPeter <peterdonat.bme@gmail.com>
2021-06-13 22:40:10 +0200

    add type for creating order

--- src/types/orders.type.ts
+++ src/types/orders.type.ts
@@ -1,7 +1,14 @@
-import { IsDefined, IsUUID } from 'class-validator';
+import { IsArray, IsDefined, IsUUID } from 'class-validator';
 
 export class GetOrderRequest {
   @IsUUID()
   @IsDefined()
   public readonly id!: string;
+}
+
+export class CreateOrderRequest {
+  @IsArray()
+  @IsDefined()
+  @IsUUID('all', { each: true })
+  public readonly itemIds!: string[];
 }
