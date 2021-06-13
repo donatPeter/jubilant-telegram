2c56b1d62f5ff90996d10b91708ec0db65813ce5
donatPeter <peterdonat.bme@gmail.com>
2021-06-13 18:23:06 +0200

    init orders module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './modules/database/database.module';
+import { OrdersModule } from './modules/orders/orders.module';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, OrdersModule],
 })
 export class AppModule {}
