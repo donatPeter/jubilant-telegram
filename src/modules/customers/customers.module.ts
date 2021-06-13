9d2d1e1785b26769c58635376b8f1925fe586f44
donatPeter <peterdonat.bme@gmail.com>
2021-06-13 21:11:37 +0200

    add db as import to customers module

--- src/modules/customers/customers.module.ts
+++ src/modules/customers/customers.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [CustomersService],
   controllers: [CustomersController],
 })
